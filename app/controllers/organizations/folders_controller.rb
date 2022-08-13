e231b5d5ce1ffa20698af01057646155b2838bea
hiroakikonno <sample@sample.com>
2022-08-13 20:04:25 +0900

    index controller/view作成

--- app/controllers/organizations/folders_controller.rb
+++ app/controllers/organizations/folders_controller.rb
@@ -1,5 +1,14 @@
 class Organizations::FoldersController < ApplicationController
   layout 'organizations'
+
   def index
+    @folders = Folder.current_owner_has(current_user)
+  end
+
+  def new
+    @folder = Folder.new
+  end
+
+  def create
   end
 end
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -2,4 +2,6 @@ class Folder < ApplicationRecord
   belongs_to :organization
   belongs_to :video
   validates :name,  presence: true, length: { maximum: 10 }
+
+  scope :current_owner_has, -> (current_user) { where(organization_id: current_user.organization_id) }
 end
