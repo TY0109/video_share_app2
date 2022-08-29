444116273a317a9934e724ed726be928b2969bee
raccoon <toshiki21320@icloud.com>
2022-08-29 11:07:31 +0900

    videosコントローラのslim化

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,10 +1,12 @@
 class VideosController < ApplicationController
-  before_action :authenticate_user!, except: :show
-  # before_actionは今後詰めていく必要あり
+  before_action :authenticate_user!, only: %i[index new create]
   before_action :set_video, only: %i[show edit update destroy]
-
+  # 投稿者本人またはオーナー→edit, update(追加予定)
+  before_action :ensure_owner, only: %i[destroy]
+  
   def index
-    @videos = Video.all
+    # n+1問題対応.includes([:video_blob])
+    @organization_videos = Video.includes([:video_blob]).current_owner_has(current_user)
   end
 
   def new
@@ -13,6 +15,7 @@ class VideosController < ApplicationController
 
   def create
     @video = Video.new(video_params)
+    @video.identify_organization_and_user(current_user)
     if @video.save
       # 今後必要に応じてファイル形式変換処理
       # ConvertVideoJob.perform_later(@video.id)
@@ -31,19 +34,27 @@ class VideosController < ApplicationController
   def update; end
 
   def destroy
-    @video.destroy
-    flash[:success] = '削除しました'
-    redirect_to videos_path
+    if @video.owner_has?(current_user) && @video.destroy
+      flash[:danger] = '動画を削除しました'
+      redirect_to videos_path
+    end
   end
 
   private
 
   def video_params
     params.require(:video).permit(:title, :video, :open_period, :range, :comment_public, :login_set, :popup_before_video,
-      :popup_after_video, :organization_id, :user_id)
+      :popup_after_video)
   end
 
   def set_video
     @video = Video.find(params[:id])
+  end
+
+  def ensure_owner
+    if current_user.role != 'owner'
+      flash[:danger] = '権限がありません'
+      redirect_to users_path
+    end
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,10 +1,24 @@
 class Video < ApplicationRecord
+  include ApplicationHelper
+
   belongs_to :organization
   belongs_to :user
 
-  # videoモデルにvideoを添付
   has_one_attached :video
 
+  validates :title, presence: true, uniqueness: { scope: :organization }
+  # videoのuniqueness設定は別の方法が必要
   validates :video, presence: true
-  validates :title, presence: true
+  
+  scope :current_owner_has, ->(current_user) { where(organization_id: current_user.organization_id) }
+
+  def identify_organization_and_user(current_user)
+    self.organization_id = current_user.organization.id 
+    self.user_id = current_user.id
+  end
+
+  def owner_has?(current_user)
+    return true if self.organization_id == current_user.organization_id
+    false
+  end
 end
