f8dfb22bc2174df1cb5551d0a35a9a7bf0370d7e
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2022-08-30 22:05:40 +0900

    jsファイル読み込めるよう修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,8 @@
+import 'bootstrap'
+import '../src/application.scss'
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
