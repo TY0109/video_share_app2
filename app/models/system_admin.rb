c134adf81a9b0f1fae9f42d63cb46e8d98e62eef
koki-kato <2k2s.work@gmail.com>
2022-09-09 21:41:16 +0900

    名前の一意性と３文字以上の制限付与

--- app/models/system_admin.rb
+++ app/models/system_admin.rb
@@ -9,5 +9,5 @@ class SystemAdmin < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :name,  presence: true, length: { in: 1..10 }
+  validates :name,  presence: true, uniqueness: true, length: { in: 3..10 }
 end
