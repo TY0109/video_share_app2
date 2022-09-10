ffbba6d0d4fd9cb9b9ea77c27e0f7168d56dbcbf
raccoon <toshiki21320@icloud.com>
2022-09-11 03:40:24 +0900

    動画情報の編集

---
+++ app/helpers/videos_helper.rb
@@ -0,0 +1,35 @@
+module VideosHelper
+
+  def selected_before1
+    return 1 if @video.range==true
+  end
+
+  def selected_before2
+    return 1 if @video.comment_public == true
+  end
+
+  def selected_before3
+    return 1 if @video.login_set == true
+  end
+
+  def selected_before4
+    return 1 if @video.popup_before_video ==true
+  end
+
+  def selected_before5
+    return 1 if @video.popup_after_video == true
+  end
+
+
+  def my_upload?(current_user)
+    return true if @video.user_id == current_user.id
+
+    false
+  end
+
+  def ensure_owner?(current_user)
+    return true if current_user.role == 'owner'
+    
+    false
+  end
+end
