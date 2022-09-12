e3e8eb2ec9deada8d0c1b10cbdd07e6306f96bb2
raccoon <toshiki21320@icloud.com>
2022-09-12 12:19:05 +0900

    記述修正

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -32,21 +32,4 @@ FactoryBot.define do
       video_test.video.attach(io: File.open('spec/fixtures/files/rec.webm'), filename: 'rec.webm', content_type: 'video/webm')
     end
   end
-
-  factory :video_other_owner, class: 'Video' do
-    title { 'ITビデオ' }
-    open_period { 'Sun, 14 Aug 2022 18:06:00.000000000 JST +09:00' }
-    range { false }
-    comment_public { false }
-    popup_before_video { false }
-    popup_after_video { false }
-    organization_id { 2 }
-    user_id { 2 }
-    organization
-    user
-
-    after(:build) do |video_other_owner|
-      video_other_owner.video.attach(io: File.open('spec/fixtures/files/rec.webm'), filename: 'rec.webm', content_type: 'video/webm')
-    end
-  end
 end
--- spec/system/videos_spec.rb
+++ spec/system/videos_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.xdescribe 'VideosSystem', type: :system, js: true do
-  let(:system_admin) { create(:system_admin) }
+  let(:system_admin) { create(:system_admin, confirmed_at: Time.now) }
   let(:organization) { create(:organization) }
   let(:another_organization) { create(:another_organization) }
   let(:user_owner) { create(:user_owner, organization_id: organization.id, confirmed_at: Time.now) }
@@ -9,9 +9,6 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
   let(:user) { create(:user, organization_id: organization.id, confirmed_at: Time.now) }
   let(:video_sample) { create(:video_sample, organization_id: user_owner.organization.id, user_id: user_owner.id) }
   let(:video_test) { create(:video_test, organization_id: user.organization.id, user_id: user.id) }
-  let(:video_other_owner) do
-    create(:video_other_owner, organization_id: another_user_owner.organization.id, user_id: another_user_owner.id)
-  end
 
   before(:each) do
     system_admin
@@ -22,13 +19,12 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
     user
     video_sample
     video_test
-    video_other_owner
   end
 
   describe '正常' do
     describe '動画一覧ページ' do
       before(:each) do
-        sign_in user_owner || sign_in system_admin
+        sign_in user_owner || system_admin
         visit videos_path(organization_id: organization.id)
       end
 
@@ -40,7 +36,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
       end
 
       it '動画削除' do
-        # find(:xpath, '/html/body/div[1]/a[2]').click
+        find(:xpath, '//*[@id="videos-index"]/div[1]/div[1]/div[2]/div[1]/a[2]').click
         expect {
           expect(page.driver.browser.switch_to.alert.text).to eq '削除しますか？'
           page.driver.browser.switch_to.alert.accept
@@ -49,7 +45,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
       end
 
       it '動画削除キャンセル' do
-        # find(:xpath, '/html/body/div[1]/a[2]').click
+        find(:xpath, '//*[@id="videos-index"]/div[1]/div[1]/div[2]/div[1]/a[2]').click
         expect {
           expect(page.driver.browser.switch_to.alert.text).to eq '削除しますか？'
           page.driver.browser.switch_to.alert.dismiss
@@ -59,7 +55,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
 
     describe '動画詳細' do
       before(:each) do
-        sign_in user_owner || user || sign_in system_admin
+        sign_in user_owner || user || system_admin
         visit video_path(video_test)
       end
 
@@ -94,8 +90,8 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
 
       it '設定を変更で動画情報が更新される' do
         # fill_inの値は、ビューのフォームのfieldのid
-        fill_in 'title_edit', with: 'サンプルビデオ２'
-        # fill_in 'open_period', with: 'Sun, 14 Aug 2022 18:06:00.000000000 JST +09:00'
+        fill_in 'title', with: 'サンプルビデオ２'
+        # fill_in 'open_period_edit', with: 'Sun, 14 Aug 2022 18:06:00.000000000 JST +09:00'
         expect(page).to have_selector '#range_edit', text: false
         expect(page).to have_selector '#comment_public_edit', text: false
         expect(page).to have_selector '#login_set_edit', text: false
@@ -113,7 +109,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
       end
 
       it 'レイアウト' do
-        expect(page).to have_link '新規投稿'
+        expect(page).to have_button '新規投稿'
         expect(page).to have_field 'title'
         expect(page).to have_field 'post'
         expect(page).to have_field 'open_period'
@@ -136,7 +132,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
         expect(page).to have_selector '#popup_before_video', text: false
         expect(page).to have_selector '#popup_after_video', text: false
         click_button '新規投稿'
-        expect(page).to have_current_path "/videos/4", ignore_query: true
+        expect(page).to have_current_path video_path(Video.last), ignore_query: true
         expect(page).to have_text '動画を投稿しました'
       end
     end
@@ -200,7 +196,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
     describe '動画一覧画面' do
       before(:each) do
         sign_in user
-        visit videos_path((organization_id: organization.id))
+        visit videos_path(organization_id: organization.id)
       end
       
       it 'レイアウトに削除なし' do
@@ -211,7 +207,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
     
     describe 'モーダル画面' do
       before(:each) do
-        sign_in user || sign_in system_admin
+        sign_in user || system_admin
         visit video_path(video_sample)
         click_link('設定')
       end
@@ -220,7 +216,7 @@ RSpec.xdescribe 'VideosSystem', type: :system, js: true do
         expect(page).to have_selector('.modal')
       end
 
-      it 'レイアウト' do
+      it 'レイアウトに設定を変更なし' do
         expect(page).to have_button '閉じる'
         expect(page).to have_field 'title_edit'
         expect(page).to have_field 'open_period_edit'
