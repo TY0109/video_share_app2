e2e44f0b27f454344c0d6b72c562f734f97e1eb9
Hiroaki Konno <all_for_joy_h0708@yahoo.co.jp>
2022-09-18 00:31:57 +0900

    uodate機能修正

--- app/javascript/packs/organizations/folder.js
+++ app/javascript/packs/organizations/folder.js
@@ -1,30 +1,33 @@
 document.addEventListener("turbolinks:load", function() {
-jQuery(function($){
-  $('th').click(function(){
-    if(!$(this).hasClass('on')){
-      $(this).addClass('on');
-      var txt = $(this).text();
-      const OrganizationId = $(this).attr('id');
-      const folderId = $(this).attr('class');
-      $(this).html('<input type="text" value='+txt+' style="width: 150px;">');
-      $('th > input').focus().blur(function(){
-        var inputVal = $(this).val();
-        if(inputVal===''){
-            inputVal = this.defaultValue;
-        };
-        $(this).parent().removeClass('on').text(inputVal);
+  jQuery(function($){
+    const UserId = $('tr').attr('id');
+    if(UserId != ""){
+      $('th').click(function(){
+        if(!$(this).hasClass('on')){
+          $(this).addClass('on');
+          var txt = $(this).text();
+          const OrganizationId = $(this).attr('id');
+          const folderId = $(this).attr('class');
+          $(this).html('<input type="text" value='+txt+' style="width: 150px;">');
+          $('th > input').focus().blur(function(){
+            var inputVal = $(this).val();
+            if(inputVal===''){
+                inputVal = this.defaultValue;
+            };
+            $(this).parent().removeClass('on').text(inputVal);
 
-        $.ajax({                         
-          url: '/organizations/'+OrganizationId+'/folders/' + folderId,
-          type: 'PATCH',                
-          data: {                        
-            folder: {
-              name: inputVal
-            }
-          }
-        })
+            $.ajax({                         
+              url: '/organizations/'+OrganizationId+'/folders/' + folderId,
+              type: 'PATCH',                
+              data: {                        
+                folder: {
+                  name: inputVal
+                }
+              }
+            })
+          });
+        };
       });
     };
   });
-});
 })
