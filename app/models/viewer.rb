e53be08d288ae8220aa25c1406d04bc29094c350
koki-kato <2k2s.work@gmail.com>
2022-09-21 20:45:35 +0900

    scopeの定義変更

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -12,4 +12,14 @@ class Viewer < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :name,  presence: true, length: { in: 1..10 }
+
+  # 引数のorganization_idと一致するviewerの絞り込み
+  scope :current_owner_has, lambda { |current_user|
+                              includes(:organization_viewers).where(organization_viewers: { organization_id: current_user.organization_id })
+                            }
+  scope :viewer_has, lambda { |organization_id|
+                       includes(:organization_viewers).where(organization_viewers: { organization_id: organization_id })
+                     }
+  # 退会者は省く絞り込み
+  scope :subscribed, -> { where(is_valid: true) }
 end
