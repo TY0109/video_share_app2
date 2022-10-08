083795c9cc582f4ab37d3993fa9155efbf01a1c6
raccoon <toshiki21320@icloud.com>
2022-10-09 01:41:07 +0900

    ファクトリーボット実際のvimeoへの動画アップをコメントアウト

--- spec/factories/videos.rb
+++ spec/factories/videos.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :video_sample, class: 'Video' do
-    
     title { 'サンプルビデオ' }
     open_period { 'Sun, 14 Aug 2022 18:06:00.000000000 JST +09:00' }
     range { false }
@@ -12,18 +11,18 @@ FactoryBot.define do
     user_id { 1 }
     organization
     user
-     # vimeoへの動画データのアップロードは行わず。(vimeoに動画データがなくても、data_urlを仮で設定しておけば、アプリ内ではインスタンスが存在可能)
+    # vimeoへの動画データのアップロードは行わず。(vimeoに動画データがなくても、data_urlを仮で設定しておけば、アプリ内ではインスタンスが存在可能)
     data_url { '/videos/111111111' }
-    
+
     # requestsとsystemのdestroyのテスト用に、実際にvimeoに動画データをアップロードする。destroyのテストをコメントアウトしているため、こちらもコメントアウト
-    after(:build) do |video_sample|
-      video = File.open('spec/fixtures/files/rec.webm')
-      video_client = VimeoMe2::User.new(ENV['VIMEO_API_TOKEN'])
-      video_data = video_client.upload_video(video)
-      video_sample.data_url = video_data['uri']
-    end
+    # after(:build) do |video_sample|
+    #   video = File.open('spec/fixtures/files/rec.webm')
+    #   video_client = VimeoMe2::User.new(ENV['VIMEO_API_TOKEN'])
+    #   video_data = video_client.upload_video(video)
+    #   video_sample.data_url = video_data['uri']
+    # end
   end
-  
+
   factory :video_test, class: 'Video' do
     title { 'テストビデオ' }
     open_period { 'Sun, 14 Aug 2022 18:06:00.000000000 JST +09:00' }
