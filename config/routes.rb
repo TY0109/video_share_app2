f1200eecb0921d7be2450ce27380d36f52405aab
raccoon <toshiki21320@icloud.com>
2022-11-27 21:02:39 +0900

    フォルダ内の動画の削除

--- config/routes.rb
+++ config/routes.rb
@@ -71,7 +71,12 @@ Rails.application.routes.draw do
   root 'use#top'
   # =================================================================
 
-  resources :videos
+  resources :videos do
+    scope module: :videos do
+      resources :video_folders, only: :destroy
+    end
+  end
+
   # 動画の論理削除(データは残すが表示しないという意味でhiddensコントローラと命名)
   scope module: :videos do
     get 'videos/:id/hidden' => 'hiddens#confirm', as: :videos_hidden
