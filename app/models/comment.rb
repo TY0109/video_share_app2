5529d9b429b973a71071818a8471a6a26031b55d
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2022-12-28 15:46:58 +0900

    setメソッドをモデルへ移動

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,4 +9,15 @@ class Comment < ApplicationRecord
 
   # バリデーション
   validates :comment, presence: true
+
+  # コメントしたアカウントidをセット
+  def set_commenter_id
+    if current_system_admin && (@account == current_system_admin)
+      @comment.system_admin_id = current_system_admin.id
+    elsif current_user && (@account == current_user)
+      @comment.user_id = current_user.id
+    elsif current_viewer && (@account == current_viewer)
+      @comment.viewer_id = current_viewer.id
+    end
+  end
 end
--- app/models/reply.rb
+++ app/models/reply.rb
@@ -7,4 +7,15 @@ class Reply < ApplicationRecord
 
   # バリデーション
   validates :reply, presence: true
+
+  # コメント返信したアカウントのidをセット
+  def set_replyer_id
+    if current_system_admin && (@account == current_system_admin)
+      @reply.system_admin_id = current_system_admin.id   
+    elsif current_user && (@account == current_user)
+      @reply.user_id = current_user.id
+    elsif current_viewer && (@account == current_viewer)
+      @reply.viewer_id = current_viewer.id
+    end
+  end
 end
