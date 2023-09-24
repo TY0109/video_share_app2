3e2cbb0d9aaa9c16b920a4f9e414df5414798f5d
Ryousuke-Tanaka55 <tianzhongliangyou312@gmail.com>
2023-09-24 22:17:57 +0900

    モデルスペック文言修正、改行追加

--- spec/models/video_search_spec.rb
+++ spec/models/video_search_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Video, type: :model do
     describe 'search' do
       context '満たすデータが存在する場合' do
         context '検索フォームが未入力の場合' do
-          it '組織内の動画をすべて含む配列を返すこと' do
+          it 'すべての動画を含む配列を返すこと' do
             expect(Video.search('')).to include(video_jan_public_owner, video_feb_private_owner, video_mar_public_staff,
                video_apr_private_staff, video_may_public_staff1)
           end
@@ -40,9 +40,8 @@ RSpec.describe Video, type: :model do
             expect(Video.search(title_like: 'テ')).to include(video_jan_public_owner, video_feb_private_owner, video_mar_public_staff,
                video_apr_private_staff, video_may_public_staff1)
           end
-          it 'タイトル「テスト動画1月」以外の配列は返さないこと' do
-            expect(Video.search(title_like: 'テスト動画1月')).to_not include(video_feb_private_owner, video_mar_public_staff,
-               video_apr_private_staff, video_may_public_staff1)
+          it 'タイトル「テスト動画1月」を含む配列を返すこと' do
+            expect(Video.search(title_like: 'テスト動画1月')).to include(video_jan_public_owner)
           end
         end
         context '公開期間開始日時が2023-02-01 00:00場合' do
@@ -51,30 +50,33 @@ RSpec.describe Video, type: :model do
                video_apr_private_staff, video_may_public_staff1)
           end
         end
-        context '公開期間開始日時が2023-01-31 23:59の場合' do
+        context '公開期間終了日時が2023-01-31 23:59の場合' do
           it '2023-01-31 23:59以前の配列を含むこと' do
             expect(Video.search(open_period_to: '2023-01-31T23:59')).to include(video_jan_public_owner)
           end
         end
         context '公開範囲「すべての動画」を選択した場合' do
-          it 'すべての動画の配列を返すこと' do
+          it 'すべての動画を含む配列を返すこと' do
             expect(Video.search(range: 'all')).to include(video_jan_public_owner, video_feb_private_owner, video_mar_public_staff,
                video_apr_private_staff, video_may_public_staff1)
           end
         end
         context '公開範囲「一般公開のみ」を選択した場合' do
-          it '一般公開の配列を返すこと' do
+          it '「一般公開のみ」の配列を返すこと' do
             expect(Video.search(range: 'true')).to include(video_jan_public_owner, video_mar_public_staff, video_may_public_staff1)
           end
         end
         context '公開範囲「限定公開のみ」を選択した場合' do
-          it '限定公開のみの配列を返すこと' do
+          it '「限定公開のみ」の配列を返すこと' do
             expect(Video.search(range: 'false')).to include(video_feb_private_owner, video_apr_private_staff)
           end
         end
         context '動画投稿者を満たす場合' do
           it '動画投稿者「オ」を含む配列を返すこと' do
             expect(Video.search(user_name: 'オ')).to include(video_jan_public_owner, video_feb_private_owner)
+          end
+          it '動画投稿者「オ」を含まない配列を返さないこと' do
+            expect(Video.search(user_name: 'オ')).to_not include(video_mar_public_staff, video_apr_private_staff, video_may_public_staff1)
           end
         end
         context 'タイトル、公開期間開始日時ともに満たす場合' do
@@ -161,7 +163,7 @@ RSpec.describe Video, type: :model do
           end
         end
         context 'タイトル、公開期間、動画投稿者すべてを満たす場合' do
-          it 'タイトル「テスト動画1」かつ2023-01-01 00:00以降 2023-01-31 23:59以前かつ動画投稿者「オーナー」の配列を返すこと' do
+          it 'タイトル「テスト動画1月」かつ2023-01-01 00:00以降 2023-01-31 23:59以前かつ動画投稿者「オーナー」の配列を返すこと' do
             expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59',
                user_name: 'オーナー')).to include(video_jan_public_owner)
           end
@@ -172,8 +174,8 @@ RSpec.describe Video, type: :model do
               video_jan_public_owner)
           end
         end
-        context 'すべてを満たす場合' do
-          it 'タイトル「テスト動画1月」かつ2023-01-01 00:00以降 2023-01-31 23:59以前かつ「すべての動画」かつ動画投稿者「オーナー」の配列を返すこと' do
+        context 'タイトル、公開期間、公開範囲、動画投稿者すべてを満たす場合' do
+          it 'タイトル「テスト動画1月」かつ2023-01-01 00:00以降 2023-01-31 23:59以前かつ「一般公開のみ」かつ動画投稿者「オーナー」の配列を返すこと' do
             expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59',
                range: 'true', user_name: 'オーナー')).to include(video_jan_public_owner)
           end
@@ -181,7 +183,7 @@ RSpec.describe Video, type: :model do
       end
 
       context '満たすデータが存在しない場合' do
-        context 'タイトルが満たさない場合' do
+        context 'タイトルを満たさない場合' do
           it '空の配列を返すこと' do
             expect(Video.search(title_like: 'テスト動画10月')).to be_empty
           end
@@ -225,11 +227,6 @@ RSpec.describe Video, type: :model do
           end
           it '公開期間終了日時のみ満たす場合、空の配列を返すこと' do
             expect(Video.search(title_like: 'テスト動画10月', open_period_to: '2023-01-31T23:59')).to be_empty
-          end
-        end
-        context 'タイトル、公開期間終了日時どちらも満たさない場合' do
-          it '空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_to: '2022-12-31T23:59')).to be_empty
           end
         end
         context 'タイトル、公開範囲どちらかのみ満たす場合' do
@@ -296,68 +293,95 @@ RSpec.describe Video, type: :model do
           end
         end
         context '公開範囲、動画投稿者どちらかのみ満たす場合' do
-          it '公開範囲のみ満たす場合、からの配列を返すこと' do
+          it '公開範囲のみ満たす場合、空の配列を返すこと' do
             expect(Video.search(range: 'all', user_name: 'オーナー10')).to be_empty
+          end
+          it '動画投稿者のみ満たす場合、空の配列を返すこと' do
+            expect(Video.search(range: 'false', user_name: 'スタッフ1')).to be_empty
           end
         end
         context 'タイトル、公開期間、公開範囲いずれかのみ満たす場合' do
           it 'タイトルのみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00', open_period_to: '2023-01-31T23:58', range: 'false')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2023-01-31T23:58', range: 'false')).to be_empty
           end
           it '公開期間のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'false')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', range: 'false')).to be_empty
           end
           it '公開範囲のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', range: 'all')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2022-12-31T23:59', range: 'all')).to be_empty
           end
           it 'タイトル、公開期間のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'false')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', range: 'false')).to be_empty
           end
           it 'タイトル、公開範囲のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', range: 'true')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2022-12-31T23:59', range: 'true')).to be_empty
           end
           it '公開期間、公開範囲のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'true')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', range: 'true')).to be_empty
+          end
+        end
+        context 'タイトル、公開期間、動画投稿者いずれも満たさない場合' do
+          it '空の配列を返すこと' do
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2022-12-31T23:59', user_name: 'オーナー10')).to be_empty
           end
         end
         context 'タイトル、公開期間、動画投稿者いずれかのみ満たす場合' do
           it 'タイトルのみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00', open_period_to: '2023-01-31T23:58', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2023-01-31T23:58', user_name: 'オーナー10')).to be_empty
           end
           it '公開期間のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', user_name: 'オーナー10')).to be_empty
           end
           it '動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', user_name: 'オーナー')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2022-12-31T23:59', user_name: 'オーナー')).to be_empty
           end
           it 'タイトル、公開期間のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', user_name: 'オーナー10')).to be_empty
           end
           it 'タイトル、動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', user_name: 'オーナー')).to be_empty
+            expect(Video.search(title_like: 'テスト動画1月', open_period_from: '2023-06-01T00:00',
+               open_period_to: '2022-12-31T23:59', user_name: 'オーナー')).to be_empty
           end
           it '公開期間、動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', user_name: 'オーナー')).to be_empty
+            expect(Video.search(title_like: 'テスト動画10月', open_period_from: '2023-01-01T00:00',
+               open_period_to: '2023-01-31T23:59', user_name: 'オーナー')).to be_empty
           end
         end
         context '公開期間、公開範囲、動画投稿者いずれかのみ満たす場合' do
           it '公開期間のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'true', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59',
+               range: 'false', user_name: 'オーナー10')).to be_empty
           end
           it '公開範囲のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', range: 'all', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59',
+               range: 'all', user_name: 'オーナー10')).to be_empty
           end
           it '動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', range: 'false', user_name: 'オーナー')).to be_empty
+            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59',
+               range: 'false', user_name: 'スタッフ1')).to be_empty
           end
           it '公開期間、公開範囲のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'true', user_name: 'オーナー10')).to be_empty
+            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59',
+               range: 'true', user_name: 'オーナー10')).to be_empty
           end
           it '公開期間、動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59', range: 'false', user_name: 'オーナー')).to be_empty
+            expect(Video.search(open_period_from: '2023-01-01T00:00', open_period_to: '2023-01-31T23:59',
+               range: 'false', user_name: 'オーナー')).to be_empty
           end
           it '公開範囲、動画投稿者のみ満たす場合、空の配列を返すこと' do
-            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59', range: 'true', user_name: 'オーナー')).to be_empty
+            expect(Video.search(open_period_from: '2023-06-01T00:00', open_period_to: '2022-12-31T23:59',
+               range: 'true', user_name: 'オーナー')).to be_empty
           end
         end
         context '公開範囲を除き満たさない場合' do
