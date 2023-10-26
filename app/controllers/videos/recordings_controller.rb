9ffabbf56a777d65c5d2fdaa300a589ebe8ad748
ryusuke <ryusuke1133@gmail.com>
2023-10-26 17:47:29 +0900

    遷移制限実装

--- app/controllers/videos/recordings_controller.rb
+++ app/controllers/videos/recordings_controller.rb
@@ -1,5 +1,15 @@
 class Videos::RecordingsController < ApplicationController
+  before_action :ensure_current_user
   layout 'recordings'
 
   def new;end
+
+  private
+  # オーナー、動画投稿者のみ許可
+  def ensure_current_user
+    unless current_user?
+      flash[:danger] = '権限がありません。'
+      redirect_back(fallback_location: root_url)
+    end
+  end
 end
